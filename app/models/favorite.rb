d542a929f40c640fc6b1e66f7bdd8126570114c6
mattgoar <mattgoar@gmail.com>
2015-08-19 13:49:43 -0500

    Completed parts 2 & 3

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,4 +2,7 @@ class Favorite < ActiveRecord::Base
     validates :user_id, presence: true
     validates :dish_id, presence: true
     validates :venue_id, presence: true
+    belongs_to :user
+    belongs_to :venue
+    belongs_to :dish
 end
--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,3 +1,4 @@
 class Neighborhood < ActiveRecord::Base
    validates :name, presence: true, :uniqueness => { :scope => :city }
+   has_many :venues
 end
